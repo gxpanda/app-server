b1880e0dec2d6bd6f75cefbf7e24940de4d52806
Xu Gao <xg2z7@mst.edu>
2018-10-06 23:30:40 -0700

    update language

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/',function(req,res){
-	res.send('Hello Panda\'s First App Server');
+	res.send('Hello Panda\'s First App Server!');
 });
 
 app.listen(3000);
